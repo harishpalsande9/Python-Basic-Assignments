6c7d155f7a8a817a879b27ef70ab0447bf2bb363
harishpalsande9 <harishpalsande9>
2022-06-24 11:27:21 +0530

    Assignment 1

---
+++ Assignment - 1/assignment-1.py
@@ -0,0 +1,10 @@
+print("1) In the below elements which of them are values or an expression? eg:- values can be integer or string and expressions will be mathematical operators. =>  Answer ====>")
+print("Hello Is a String")
+print("-87.8 Is a Float")
+print("+ Is a mathematical operators")
+print("6  Is a Number")
+
+print("2) What is the difference between string and variable?  =>  Answer ====>")
+x = "Harish Palsande"
+print("Eg X is String the Variable, and we declared it as a String, use the single=to assign the text to it. ")
+print("x =", x)
