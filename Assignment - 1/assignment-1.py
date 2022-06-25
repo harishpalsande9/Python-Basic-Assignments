b622fb5ab7d24591a52d6f3997bb569d8a667b77
harishpalsande9 <harishpalsande9>
2022-06-25 17:55:57 +0530

    Assignment 1 Complet

--- Assignment - 1/assignment-1.py
+++ Assignment - 1/assignment-1.py
@@ -1,10 +1,71 @@
-print("1) In the below elements which of them are values or an expression? eg:- values can be integer or string and expressions will be mathematical operators. =>  Answer ====>")
+print("1. In the below elements which of them are values or an expression? eg:- values can be integer or string and expressions will be mathematical operators. =>  Answer ====>")
 print("Hello Is a String")
 print("-87.8 Is a Float")
 print("+ Is a mathematical operators")
 print("6  Is a Number")
 
-print("2) What is the difference between string and variable?  =>  Answer ====>")
+
+print("2. What is the difference between string and variable?  =>  Answer ====>")
 x = "Harish Palsande"
 print("Eg X is String the Variable, and we declared it as a String, use the single=to assign the text to it. ")
 print("x =", x)
+
+
+print("3. Describe three different data types.   =>  Answer ====>")
+print("In thear Python Are Built-in  Data Types  ")
+print("Following Are Data Types ")
+print("1) Test Type :- str")
+print("2) Numeric Types :- int , float , complex")
+print("3)Sequence Types :- list , tupal , range ")
+print("4)Mapping Type :- dict")
+print("5)Set Type :- set , frozenset ")
+print("6)Boolean Types :- bool ")
+print("7)Biary Types :- bytes , bytearry , memoryview")
+print("8)None Type :- NoneType")
+
+x = "Hello World"                  # String
+x = 20                             # int
+x = 20.5                           # float
+x = 1j                             # complex
+x = ["apple", "banana", "cherry"]  # list
+x = ("apple", "banana", "cherry")  # tuple
+x = range(6)                       # range
+x = {"Name": "John", "Age": "36"}  # dict
+x = {"apple", "banana", "cherry"}  # set
+x = frozenset({"apple", "banana", "cherry"})  # frozenset
+x = True                           # bool
+x = b"Hello"                       # bytes
+x = bytearray(5)                   # bytearray
+x = memoryview(bytes(5))           # memoryview
+x = None                           # NoneType
+# This Are The Data Type
+
+
+print("4. What is an expression made up of? What do all expressions do?")
+print("An expression is a combination of operators and operands that is interpreted to produce some other value. In any programming language, an expression is evaluated as per the precedence of its operators.")
+
+
+print("5. This assignment statements, like spam = 10. What is the difference between an expression and a statement?")
+print("In programming language terminology, an “expression” is a combination of values and functions that are combined and interpreted by the compiler to create a new value, as opposed to a “statement” which is just a standalone unit of execution and doesn’t return anything.")
+
+
+print("6. After running the following code, what does the variable bacon contain? bacon =22 bacon + 1")
+print("Answer Is 23")
+
+
+print("7. What should the values of the following two terms be? 'spam' + 'spamspam' 'spam' * 3")
+print("Answer Is spamspamspam Or spamspamspam")
+
+# 8. Why is eggs a valid variable name while 100 is invalid?
+
+# Ans: because we can't start giving variable an integer name. if we, we should begin with, a string-like alphabet name then integer. e100 or eggs100 is valid
+
+# 9. What three functions can be used to get the integer, floating-point number, or string version of a value?
+
+# ans: str(), int(), float()
+
+# 10. Why does this expression cause an error? How can you fix it?
+
+# 'I have eaten' + 99 + 'burritos'
+
+# sol:  because 99 is an integer it cannot be concatenated with strings, if we have to concatenate it we need to do typecasting.
